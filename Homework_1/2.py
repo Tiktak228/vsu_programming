72519799be7772a7774fbe21d46c7f8ce5af3b5b
Tiktak228 <49941257+Tiktak228@users.noreply.github.com>
2019-09-22 13:38:18 +0300

    Create 2.py

---
+++ Homework_1/2.py
@@ -0,0 +1,11 @@
+a = float(input("Vvedite a"))
+b = float(input("Vvedite b"))
+c = float(input("Vvedite c"))
+f = float(input("Vvedite f"))
+d = float(input("Vvedite d"))
+k = f-d
+
+if k == 0:
+    print("delit' na nil' nel'zia")
+else:
+    print((a * b - c) / (f - d))
